fe1453c6bc93b931566e754311b62099191fd90c
Luca Menichetti <meniluca@gmail.com>
2012-04-01 20:49:24 +0200

    esercizi array

--- array-functions.js
+++ array-functions.js
@@ -18,6 +18,7 @@ var evenFilter = function (array) {
 
 evenFilter([1,2,3,4,5,6,7,8,9,10]);
 
+
 /*
 	Riscrivere il prototipo degli arrai in modo tale da poter invocare la funzione in questo modo
 	[1,2,3,4,5,6,7,8,9,10].evenFilter();
@@ -52,18 +53,20 @@ function sumAllPositive(array) {
 
 sumAllPositive([-1,2,-3,4,-5,6,-7,8,-9,10]);
 
+
 /*
 	Scrivere una funzione chiamata apply
 	questa funzione prende come parametro una coppia di funzioni
 	e restituiesce il valore della seconda applicata alla prima
+	return f(g())
 */
 
-var apply = function (array) {
-	if (typeof array[0] == "undefined")
-		return array[1]();
-	if (typeof array[1] == "undefined")
-		return array[0]();
-	return array[0](array[1]());
+var apply = function (pair) {
+	if (typeof pair[0] == "undefined")
+		return pair[1];
+	if (typeof pair[1] == "undefined")
+		return pair[0];
+	return pair[0](pair[1]);
 }
 
 function funzioneA(fun) {
@@ -78,7 +81,21 @@ function funzioneB(fun) {
 	return "Questa è la funzione B che chiama la prossima funzione: " + fun;
 }
 
-apply([funzioneA,funzioneB]);
-apply([funzioneB,funzioneA]);
-apply([funzioneA]);
-apply([,funzioneB]);
+apply([funzioneA,funzioneB()]);
+apply([funzioneB,funzioneA()]);
+apply([funzioneA()]);
+apply([,funzioneB()]);
+
+
+/*
+	Scrivere una funzione applyAll che prende come parametro una funzione f
+	e un array di funzioni g e ritorni un array contenente i risultati della funzione g[i] applicati a f
+	return [f(g[0],f(g[1]),...]
+*/
+
+var applyAll = function (fun, array){
+	return array.map( function (curr) { return apply([fun,curr]) } );
+}
+
+applyAll(Math.cos,[Math.cos(1),Math.sin(1),Math.cos(2),Math.sin(2),Math.cos(3),Math.sin(3)])
+
