d1f726eb398b9c55bfa7f3aa4b1398085d1797ad
Luca Menichetti <meniluca@gmail.com>
2012-04-27 10:40:08 +0200

    extrude

--- 27-04/esercizio1.js
+++ 27-04/esercizio1.js
@@ -2,10 +2,12 @@ var threeBuilding = POLYLINE([[0,0],[0,9],[3,9],[3,8],[1,8],[1,5],[3,5],[3,4],[1
 
 var secondThreeBuilding = T([0,1])([7,9])(R([0,1])(PI)(threeBuilding));
 
+var building = STRUCT([threeBuilding , secondThreeBuilding]);
 
-//DRAW(threeBuilding);
+DRAW(building);
 
-//DRAW(secondThreeBuilding);
+HIDE(building);
 
-var building = STRUCT([[threeBuilding],[secondThreeBuilding]]);
+var extrudedBuilding = EXTRUDE([2])(building);
 
+DRAW(extrudedBuilding);
