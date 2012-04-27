2a233c02f6a92c6ad70455446b7b935a3134b35c
Luca Menichetti <meniluca@gmail.com>
2012-04-27 10:15:59 +0200

    pratica 27-4-2012

---
+++ H.js
@@ -0,0 +1,10 @@
+/*
+
+	Scrivere una funzione H del secondo ordine
+	che ritorna la matrice di scorrimento
+
+	H([])([])
+
+	H(array di interi le coordinate che voglio cambiare)([array di coefficienti di scorrimento])
+
+*/
