dad1e170b8953193c05b9b63f63e4ef6de3ebef7
Luca Menichetti <meniluca@gmail.com>
2012-05-03 11:26:49 +0200

    update

--- 27-04/curvesDrawer.js
+++ 27-04/curvesDrawer.js
@@ -9,24 +9,29 @@
 var PARTS;
 var SELECTOR;
 
-function drawCubicHermite(controlPoints, parts, color, selector) {
+function drawCubicHermite(controlPoints, color, drawPolyline, drawControlPoints) {
 
-	var parts = parts || 10;
 	var color = color || [0,0,0,1];
-	var selector = selector || S0;
+	
+	var selector = this.SELECTOR || S0;
+	var parts = this.PARTS || 10;
 
 	var domain = INTERVALS(1)(parts);
 
 	var curve = MAP(CUBIC_HERMITE(selector)(controlPoints))(domain);
 	var curve = COLOR(color)(curve);
 
-	var points = getModelControlPoints([controlPoints[0],controlPoints[1]]);
+	if ( drawControlPoints === "yes" ) {
+		var points = getModelControlPoints([controlPoints[0],controlPoints[1]]);
+		curve = STRUCT([curve, points]);
+	}
 
-	var polyline = POLYLINE([controlPoints[0],controlPoints[1]]);
+	if ( drawPolyline === "yes" ) {
+		var polyline = POLYLINE([controlPoints[0],controlPoints[1]]);
+		curve = STRUCT([curve, polyline]);
+	}
 
-	var struct = STRUCT([curve, points, polyline]);
-
-	return DRAW(struct);
+	return DRAW(curve);
 
 }
 
@@ -35,16 +40,6 @@ function drawBezier(controlPoints, color, drawPolyline, drawControlPoints) {
 
 	var color = color || [0,0,0,1];
 	
-	if ( drawControlPoints === "yes" || drawControlPoints === "no" ) {
-		var drawControlPoints = drawControlPoints;
-	} else
-		var drawControlPoints = "no";
-	
-	if ( drawPolyline === "yes" || drawPolyline === "no" ) {
-		var drawPolyline = drawPolyline;
-	} else
-		var drawPolyline = "no";
-
 	var selector = this.SELECTOR || S0;
 	var parts = this.PARTS || 10;
 
@@ -53,19 +48,17 @@ function drawBezier(controlPoints, color, drawPolyline, drawControlPoints) {
 	var curve = MAP(BEZIER(selector)(controlPoints))(domain);
 	var curve = COLOR(color)(curve);
 
-	var struct = curve;
-
 	if ( drawControlPoints === "yes" ) {
 		var points = getModelControlPoints(controlPoints);
-		struct = STRUCT([struct, points]);
+		curve = STRUCT([curve, points]);
 	}
 
 	if ( drawPolyline === "yes" ) {
 		var polyline = POLYLINE(controlPoints);
-		struct = STRUCT([struct, polyline]);
+		curve = STRUCT([curve, polyline]);
 	}
 
-	return DRAW(struct);
+	return DRAW(curve);
 
 }
 
